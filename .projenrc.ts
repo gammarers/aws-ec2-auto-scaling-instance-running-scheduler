00ab7fccdd9be1c7e6963aba8625ea5ba1dfe9f3
yicr <yicr@users.noreply.github.com>
2023-09-20 14:27:31 +0900

    chore: min node version set to 16.0.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -13,7 +13,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'https://github.com/gammarer/aws-ec2-auto-scaling-instance-running-scheduler.git',
   releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
-  minNodeVersion: '18.0.0',
+  minNodeVersion: '16.0.0',
   workflowNodeVersion: '18.17.1',
   depsUpgradeOptions: {
     workflowOptions: {
